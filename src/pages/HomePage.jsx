1f69d49232aeff7d71841a51c164ec39245f5fee
Laissa Cunha <laissa.cunha@ed3.com.br>
2024-02-04 13:55:24 -0300

    feat: adiciona ListItems à Home Page

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,7 +1,19 @@
 import React from "react"
+import ListItems from '../components/ListItems/ListItems'
 import styles from './HomePage.module.css'
 
 const HomePage = () => {
+  const socialMedia = [{
+    path: '/social-media/linkedin.svg',
+    linkTo: 'https://www.linkedin.com/in/laissasc/',
+    alt: 'Logo do LinkedIn'
+  },
+  {
+    path: '/social-media/github.svg',
+    linkTo: 'https://github.com/LaissaSaraiva',
+    alt: 'Logo do Github'
+  }]
+
   return (
     <div className={styles.home__background}>
       <div className={styles.home__container}>
@@ -13,6 +25,10 @@ const HomePage = () => {
           <div className={styles.title}>
             <h1>Laíssa Saraiva</h1>
             <p>Desenvolvedora Frontend</p>
+          </div>
+
+          <div className={styles.items}>
+            <ListItems items={socialMedia}/>
           </div>
         </div>
         <div className={styles.image__container}>
