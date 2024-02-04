d26034b400c2290d0c8daf2b22a7b5067c457179
Laissa Cunha <laissa.cunha@ed3.com.br>
2024-02-04 11:26:34 -0300

    feat: adiciona componente ao Footer

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,9 +1,24 @@
 import React from "react"
 import styles from './Footer.module.css'
+import ListItems from "../ListItems/ListItems";
 
 const Footer = () => {
+  const socialMedia = [{
+    path: '/social-media/linkedin.svg',
+    linkTo: 'https://www.linkedin.com/in/laissasc/',
+    alt: 'Logo do LinkedIn'
+  },
+  {
+    path: '/social-media/github.svg',
+    linkTo: 'https://github.com/LaissaSaraiva',
+    alt: 'Logo do Github'
+  }]
+
   return (
     <footer className={styles.footer}>
+      <div>
+        <ListItems items={socialMedia}/>
+      </div>
       <div className={styles.footer__container}>
         <span>
           Projeto Final do Módulo de React do{" "}
