e5b3c705c96bb1ee21ca6b951d42c159fc4a9d51
Laissa Cunha <laissa.cunha@ed3.com.br>
2024-02-04 13:24:41 -0300

    style: inicia estilização de Home Page

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,12 +1,26 @@
 import React from "react"
+import styles from './HomePage.module.css'
 
 const HomePage = () => {
+  return (
+    <div className={styles.home__background}>
+      <div className={styles.home__container}>
+        <div className={styles.description__container}>
+          <div className={styles.pre__title}>
+            <p>Olá, Eu sou</p>
+          </div>
 
-  return(
-    <>
-    <h1>Olá Mundo! Projeto Final Pretalab</h1>
-    </>
-  )
-}
+          <div className={styles.title}>
+            <h1>Laíssa Saraiva</h1>
+            <p>Desenvolvedora Frontend</p>
+          </div>
+        </div>
+        <div className={styles.image__container}>
+          <img className={styles.image} src="/imgs/profile_home.png" alt="" />
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default HomePage;
