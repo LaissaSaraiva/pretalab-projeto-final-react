57099eaf36437894688d263e16652c9c6bf23bd1
Laissa Cunha <laissa.cunha@ed3.com.br>
2024-02-14 21:47:59 -0300

    feat: inicia lista  de comentários

--- src/pages/CommentsPage/CommentsPage.jsx
+++ src/pages/CommentsPage/CommentsPage.jsx
@@ -7,6 +7,7 @@ import styles from './Comments.module.css'
 const CommentsPage = () => {
 
   const [newComment, setNewComment] = useState('');
+  // const [list, setList] = useState('')
   console.log(newComment, 'Input digitado')
 
 
@@ -21,10 +22,46 @@ const CommentsPage = () => {
 
           <div className={styles.comments__form}>
             <Forms>
-              <Textarea placeholder="Escreva seu comentário... " onChange={(event) => setNewComment((event.target.value))} value={newComment} />
+              <Textarea placeholder="Escreva seu comentário... "  onChange={(event) => setNewComment((event.target.value))} value={newComment} />
               <Button type="submit" value="+" />
             </Forms>
           </div>
+        </div>
+
+        <div className={styles.list__comments__wrapper}>
+          <ul>
+            <li>
+              <div className={styles.list__description}>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos dolore hic quibusdam rerum nisi aspernatur iure omnis tenetur, illo eligendi deserunt mollitia quam porro labore libero minima voluptates, perferendis tempora!</p>
+              </div>
+              
+              <Button className={styles.list__button} value=""/>
+            </li>
+
+            <li>
+              <div className={styles.list__description}>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos dolore hic quibusdam rerum nisi aspernatur iure omnis tenetur, illo eligendi deserunt mollitia quam porro labore libero minima voluptates, perferendis tempora!</p>
+              </div>
+              
+              <Button className={styles.list__button} value=""/>
+            </li>
+
+            <li>
+              <div className={styles.list__description}>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos dolore hic quibusdam rerum nisi aspernatur iure omnis tenetur, illo eligendi deserunt mollitia quam porro labore libero minima voluptates, perferendis tempora!</p>
+              </div>
+              
+              <Button className={styles.list__button} value=""/>
+            </li>
+
+            <li>
+              <div className={styles.list__description}>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos dolore hic quibusdam rerum nisi aspernatur iure omnis tenetur, illo eligendi deserunt mollitia quam porro labore libero minima voluptates, perferendis tempora!</p>
+              </div>
+              
+              <Button className={styles.list__button} value=""/>
+            </li>
+          </ul>
         </div>
       </section>
     </main>
